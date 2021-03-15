1b3ff551ad7d0855f4c7dbb0f61d41689d41ca39
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2021-03-15 14:28:27 +0300

    test reverse int ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let z = '';
+  for(let i = str.length - 1; i >=0; i--) {
+      z+=str[i];
+      res = parseInt(z);
+  }
+  return res;
 }
